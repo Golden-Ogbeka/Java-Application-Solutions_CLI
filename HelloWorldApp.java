7cede7577aa7f596d1b98e9370677fc61e743273
Golden Ogbeka <ogbekagolden@gmail.com>
2020-11-03 19:06:42 +0100

    Initial commit

---
+++ HelloWorldApp.java
@@ -0,0 +1,10 @@
+//This is a single line comment for a Hello World program
+/*
+		It prints "Hello World" to the console
+*/
+class HelloWorldApp{
+		public static void main(String [] args){
+				System.out.println("Hello World");
+		}
+		
+	}
